5ed9729fe1c446a2b47a95b75f15b4cbabf4fe79
Henrik <henrik.sodergren@outlook.com>
2018-11-16 13:40:33 +0100

    Added decrypt test
    Also changed signatures from String to &str

--- src/up.rs
+++ src/up.rs
@@ -40,7 +40,7 @@ impl UserPermit {
         UserPermit { hwid, id }
     }
 
-    pub fn decrypt(up: String, key: &str) -> Result<UserPermit,PermitErr> {
+    pub fn decrypt(up: &str, key: &str) -> Result<UserPermit,PermitErr> {
         if !up.chars().chain(key.chars()).all(is_hex) {
             return Err(PermitErr::NonHex);
         }
@@ -48,10 +48,10 @@ impl UserPermit {
         let (enc_hwid, _, id) = check_up_string(&up)?;
         let crypto = Blowfish::new(key.as_bytes());
         let enc = &mut [0u8; 8];
-        crypto.decrypt_block(hex::decode(enc_hwid)?.as_slice(), enc);
+        crypto.decrypt_block(hex::decode(enc_hwid)?.as_ref(), enc);
 
         Ok(UserPermit {
-            hwid : hex::encode_upper(&enc[0..5]),
+            hwid : String::from_utf8(enc[0..5].into()).unwrap(),
             id : String::from(id),
         })
     }
@@ -83,7 +83,7 @@ fn check_key(key: &str) -> Result<(), PermitErr> {
 
 // sanity checks the encrypted userpermit
 // returns the different parts of the encrypted
-fn check_up_string(up: &String) -> Result<(&str,&str,&str), PermitErr> {
+fn check_up_string(up: &str) -> Result<(&str,&str,&str), PermitErr> {
     let ul = up.len();
     if ul != PERMIT_LENGTH as usize {
         return Err(PermitErr::WrongUpLength(ul));
@@ -108,6 +108,7 @@ mod tests {
         assert_eq!("0123456789AaBbCcDdEeFfGg".chars().all(is_hex), false);
     }
 
+    // a user permit that gets encrypted and then decrypted should get back same result
     #[test]
     fn encrypt_decrypt_test() -> Result<(),PermitErr> {
         let key1 = "12345";
@@ -120,9 +121,21 @@ mod tests {
             hwid: String::from("12ab5"),
             id: String::from("1254"),
         };
-        assert_eq!(up1, UserPermit::decrypt(up1.encrypt(key1)?, key1)?);
+        assert_eq!(up1, UserPermit::decrypt(up1.encrypt(key1)?.as_str(), key1)?);
 
-        assert_eq!(up2, UserPermit::decrypt(up2.encrypt(key2)?, key2)?);
+        assert_eq!(up2, UserPermit::decrypt(up2.encrypt(key2)?.as_str(), key2)?);
+        Ok(())
+    }
+
+    #[test]
+    fn decrypt() -> Result<(), PermitErr> {
+        let key = "10121";
+        let up = "66B5CBFDF7E4139D5B6086C23130";
+        let expected = UserPermit {
+            hwid: String::from("12345"),
+            id: String::from("3130"),
+        };
+        assert_eq!(expected, UserPermit::decrypt(up, key)?);
         Ok(())
     }
 }
